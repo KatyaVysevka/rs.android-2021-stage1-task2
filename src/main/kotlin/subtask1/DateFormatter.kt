78803f069408990ccf03124a136fe300a265ff77
KatyaVysevka <katrina_v89@mail.ru>
2021-06-14 14:27:13 +0300

    task completed

--- src/main/kotlin/subtask1/DateFormatter.kt
+++ src/main/kotlin/subtask1/DateFormatter.kt
@@ -1,9 +1,26 @@
 package subtask1
 
+import java.lang.Exception
+import java.text.SimpleDateFormat
+import java.util.*
+
 class DateFormatter {
 
     // TODO: Complete the following function
     fun toTextDay(day: String, month: String, year: String): String {
-        throw NotImplementedError("Not implemented")
+       // throw NotImplementedError("Not implemented")
+        return try {
+            val dateFormat = SimpleDateFormat ("d MMMM, EEEE", Locale ("ru"))
+            val calendar = Calendar.getInstance()
+            calendar.isLenient = false
+            calendar.set(year.toInt(), month.toInt()-1 , day.toInt())
+            return dateFormat.format(calendar.time)
+
+        }
+        catch (e: Exception) {
+            "Такого дня не существует"
+        }
+
+
     }
 }
--- src/main/kotlin/subtask2/Abbreviation.kt
+++ src/main/kotlin/subtask2/Abbreviation.kt
@@ -4,6 +4,15 @@ class Abbreviation {
 
     // TODO: Complete the following function
     fun abbreviationFromA(a: String, b: String): String {
-        throw NotImplementedError("Not implemented")
+       // throw NotImplementedError("Not implemented")
+        val a1 = a.toUpperCase().toCharArray()
+        val b1 = b.toCharArray()
+        var aIndex = 0
+        var bIndex = 0
+        while (bIndex < b.count() && aIndex < a.count()) {
+            aIndex++
+            if (b1[bIndex] == a1 [aIndex]) bIndex++
+        }
+        return if (bIndex == b.count()) "YES" else "NO"
     }
 }
