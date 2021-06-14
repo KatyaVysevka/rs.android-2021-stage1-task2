b9288ee7a7e1c5df2f3f59c54d62ff9a5ba638ec
KatyaVysevka <katrina_v89@mail.ru>
2021-06-14 16:39:40 +0300

    version 7

--- src/main/kotlin/subtask2/Abbreviation.kt
+++ src/main/kotlin/subtask2/Abbreviation.kt
@@ -4,15 +4,36 @@ class Abbreviation {
 
     // TODO: Complete the following function
     fun abbreviationFromA(a: String, b: String): String {
-       // throw NotImplementedError("Not implemented")
+        // throw NotImplementedError("Not implemented")
+//        val a1 = a.toUpperCase().toCharArray()
+//        val b1 = b.toCharArray()
+//        var aIndex = 0
+//        var bIndex = 0
+//        while (bIndex < b.count() && aIndex < a.count()) {
+//            aIndex++
+//            if (b1[bIndex] == a1 [aIndex]) bIndex++
+//        }
+//        return if (bIndex == b.count()) "YES" else "NO"
         val a1 = a.toUpperCase().toCharArray()
         val b1 = b.toCharArray()
-        var aIndex = 0
-        var bIndex = 0
-        while (bIndex < b.count() && aIndex < a.count()) {
-            aIndex++
-            if (b1[bIndex] == a1 [aIndex]) bIndex++
+        val aSize = a.count() - 1
+        val bSize = b.count() - 1
+        var answerString = ""
+        var answer = ""
+        for (i in 0..bSize) {
+            if (b1[i] == a1[i]) {
+                answerString += a1[i]
+            } else {
+                for (j in i..aSize) {
+                    if (b1[i] == a1[j])
+                        answerString += a1[j]
+                }
+            }
         }
-        return if (bIndex == b.count()) "YES" else "NO"
+        if (answerString == b) answer = "YES"
+        else answer = "NO"
+        return answer
+
     }
 }
+
--- src/main/kotlin/subtask3/Blocks.kt
+++ src/main/kotlin/subtask3/Blocks.kt
@@ -11,13 +11,13 @@ class Blocks {
     fun getData(blockA: Array<*>, blockB: KClass<*>): Any {
         //throw NotImplementedError("Not implemented")
         when (blockB) {
-//            Int::class -> {
-//                var answer = 0
-//                for (i in blockA) {
-//                    if (i is Int) answer += i
-//                }
-//                return answer
-//            }
+            Int::class -> {
+                var answer = 0
+                for (i in blockA) {
+                    if (i is Int) answer += i
+                }
+                return answer
+            }
             String::class -> {
                 var answer = ""
                 for (i in blockA) {
