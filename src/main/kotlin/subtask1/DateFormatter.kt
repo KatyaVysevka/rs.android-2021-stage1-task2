142a5ec04e9a1431dc34656e810f73417c960f17
KatyaVysevka <katrina_v89@mail.ru>
2021-06-14 16:18:46 +0300

    version 5

--- src/main/kotlin/subtask1/DateFormatter.kt
+++ src/main/kotlin/subtask1/DateFormatter.kt
@@ -8,18 +8,18 @@ class DateFormatter {
 
     // TODO: Complete the following function
     fun toTextDay(day: String, month: String, year: String): String {
-         throw NotImplementedError("Not implemented")
-//        return try {
-//            val dateFormat = SimpleDateFormat("d MMMM, EEEE", Locale("ru"))
-//            val calendar = Calendar.getInstance()
-//            calendar.isLenient = false
-//            calendar.set(year.toInt(), month.toInt() - 1, day.toInt())
-//            dateFormat.format(calendar.time)
-//
-//        } catch (e: Exception) {
-//            "Такого дня не существует"
-//        }
-//
+        // throw NotImplementedError("Not implemented")
+        return try {
+            val dateFormat = SimpleDateFormat("d MMMM, EEEE", Locale("ru"))
+            val calendar = Calendar.getInstance()
+            calendar.isLenient = false
+            calendar.set(year.toInt(), month.toInt() - 1, day.toInt())
+            dateFormat.format(calendar.time)
+
+        } catch (e: Exception) {
+            "Такого дня не существует"
+        }
+
 
     }
 }
--- src/main/kotlin/subtask3/Blocks.kt
+++ src/main/kotlin/subtask3/Blocks.kt
@@ -9,35 +9,36 @@ class Blocks {
 
     // TODO: Complete the following function
     fun getData(blockA: Array<*>, blockB: KClass<*>): Any {
-        //  throw NotImplementedError("Not implemented")
-        when (blockB) {
-            Int::class -> {
-                var answer = 0
-                for (i in blockA) {
-                    if (i is Int) answer += i
-                }
-                return answer
-            }
-            String::class -> {
-                var answer = ""
-                for (i in blockA) {
-                    if (i is String) answer += i
-                }
-                return answer
-            }
-            LocalDate::class -> {
-                val dateList = mutableListOf<LocalDate>()
-                for (i in blockA) {
-                    if (i is LocalDate) {
-                        dateList.add(i)
-                    }
-                }
-                val answer = dateList.max() as LocalDate
-                val formatPattern = DateTimeFormatter.ofPattern("dd.MM.yyyy")
-                return answer.format(formatPattern)
-
-            }
-        }
-        return "Это костыль - не трогать"
+        throw NotImplementedError("Not implemented")
+//        when (blockB) {
+//            Int::class -> {
+//                var answer = 0
+//                for (i in blockA) {
+//                    if (i is Int) answer += i
+//                }
+//                return answer
+//            }
+//            String::class -> {
+//                var answer = ""
+//                for (i in blockA) {
+//                    if (i is String) answer += i
+//                }
+//                return answer
+//            }
+//            LocalDate::class -> {
+//                val dateList = mutableListOf<LocalDate>()
+//                for (i in blockA) {
+//                    if (i is LocalDate) {
+//                        dateList.add(i)
+//                    }
+//                }
+//                val answer = dateList.max() as LocalDate
+//                val formatPattern = DateTimeFormatter.ofPattern("dd.MM.yyyy")
+//                return answer.format(formatPattern)
+//
+//            }
+//        }
+//        return "Это костыль - не трогать"
     }
+
 }
