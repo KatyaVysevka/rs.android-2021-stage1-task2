93af4f77da88e74d911bc7461bb602efff0f301a
KatyaVysevka <katrina_v89@mail.ru>
2021-06-14 16:29:43 +0300

    version 6

--- src/main/kotlin/subtask3/Blocks.kt
+++ src/main/kotlin/subtask3/Blocks.kt
@@ -11,13 +11,13 @@ class Blocks {
     fun getData(blockA: Array<*>, blockB: KClass<*>): Any {
         //throw NotImplementedError("Not implemented")
         when (blockB) {
-            Int::class -> {
-                var answer = 0
-                for (i in blockA) {
-                    if (i is Int) answer += i
-                }
-                return answer
-            }
+//            Int::class -> {
+//                var answer = 0
+//                for (i in blockA) {
+//                    if (i is Int) answer += i
+//                }
+//                return answer
+//            }
             String::class -> {
                 var answer = ""
                 for (i in blockA) {
@@ -25,18 +25,18 @@ class Blocks {
                 }
                 return answer
             }
-//            LocalDate::class -> {
-//                val dateList = mutableListOf<LocalDate>()
-//                for (i in blockA) {
-//                    if (i is LocalDate) {
-//                        dateList.add(i)
-//                    }
-//                }
-//                val answer = dateList.max() as LocalDate
-//                val formatPattern = DateTimeFormatter.ofPattern("dd.MM.yyyy")
-//                return answer.format(formatPattern)
-//
-//            }
+            LocalDate::class -> {
+                val dateList = mutableListOf<LocalDate>()
+                for (i in blockA) {
+                    if (i is LocalDate) {
+                        dateList.add(i)
+                    }
+                }
+                val answer = dateList.max() as LocalDate
+                val formatPattern = DateTimeFormatter.ofPattern("dd.MM.yyyy")
+                return answer.format(formatPattern)
+
+            }
         }
         return "Это костыль - не трогать"
     }
